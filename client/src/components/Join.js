825ec544255dca0f6e48f87d64e4de3e6e04c6b4
xiaohua2003 <xcai3@ualberta.ca>
2021-04-25 22:51:52 -0600

    modify the client folder

---
+++ client/src/components/Join.js
@@ -0,0 +1,6 @@
+import React, {useState} from 'react';
+import {Link} from 'react-route-dom'
+const Join=()=>{
+    return (<h1>Join</h1>)
+}
+export default Join;
---
+++ server/index.js
@@ -0,0 +1,21 @@
+const express=require("express");
+const socketio=require('socket.io');
+const http=require('http');
+
+const port=process.env.PORT || 5000;
+const router=require('./router')
+const app=express();
+const server=http.createServer(app);
+const io=socketio(server);
+
+io.on('connection', (socket)=>{
+console.log('we have a new connection')
+socket.on('disconnect', ()=>{
+    console.log('user has left')
+})
+})
+
+app.use(router)
+server.listen(port, function(){console.log(`server is running on port ${port}`)}
+
+)
