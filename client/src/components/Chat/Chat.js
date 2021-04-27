9bf0250b349c890cbe92885bbafea3e08efae413
xiaohua2003 <xcai3@ualberta.ca>
2021-04-26 23:20:33 -0600

    add chat.css, join.css and update chat.js, join.js

---
+++ client/src/components/Chat/Chat.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import queryString from 'query-string';
+import io from 'socket.io-client';
+const Chat=()=>{
+    useEffect(()=>{})
+    return (<h1>Chat</h1>)
+}
+export default Chat;
--- client/src/components/Join.js
+++
@@ -1,6 +0,0 @@
-import React, {useState} from 'react';
-import {Link} from 'react-route-dom'
-const Join=()=>{
-    return (<h1>Join</h1>)
-}
-export default Join;
