b8b7de1364c08e54499f68d81aa4c82149a0b8d4
xiaohua2003 <xcai3@ualberta.ca>
2021-04-28 23:21:42 -0600

    update chat and index

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,10 +1,13 @@
 import React, {useState, useEffect} from 'react';
 import queryString from 'query-string';
 import io from 'socket.io-client';
+import './Chat.css'
 let socket
 const Chat=({location})=>{
     const [name, setName]=useState('');
     const [room, setRoom]=useState('');
+    const [message, setMessage]=useState('');
+    const [messages, setMessages]=useState([]);
     const ENDPOINT='localhost:5000';
     
     useEffect(()=>{
@@ -20,6 +23,28 @@ const Chat=({location})=>{
             socket.off();
         } 
     }, [ENDPOINT, location.search])
-    return (<h1>Chat</h1>)
+    //handling message
+    useEffect(()=>{
+        socket.on('message', (message)=>{
+            setMessages([...messages,message]);
+        })
+    }, [messages]);
+    //function for sending messages
+    const sendMessage=(event)=>{
+        event.preventDefault();
+        if(message){
+            socket.emit('sendMessage', message, ()=>setMessage(''))
+        }
+    }
+    console.log(message, messages)
+    return (
+    <div>
+        <div className="outerContainer">
+            <div className="container">
+              <InfoBar/>
+              {/*<input value={message} onChange={(event)=>setMessage(event.target.value)} onKeyPress={event=>event.key ==='Enter' ? sendMessage(event):null}/>*/}
+            </div>
+        </div>
+    </div>)
 }
 export default Chat;
