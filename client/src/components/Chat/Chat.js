87577770a60242091cd2ecd053f93e43a4bc4064
xiaohua2003 <xcai3@ualberta.ca>
2021-04-29 23:33:58 -0600

    update input and messages

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,7 +1,10 @@
 import React, {useState, useEffect} from 'react';
 import queryString from 'query-string';
 import io from 'socket.io-client';
-import './Chat.css'
+import './Chat.css';
+import InfoBar from '../InfoBar/InfoBar';
+import Input from '../Input/Input';
+import Messages from '../Messages/Messages'
 let socket
 const Chat=({location})=>{
     const [name, setName]=useState('');
@@ -41,8 +44,9 @@ const Chat=({location})=>{
     <div>
         <div className="outerContainer">
             <div className="container">
-              <InfoBar/>
-              {/*<input value={message} onChange={(event)=>setMessage(event.target.value)} onKeyPress={event=>event.key ==='Enter' ? sendMessage(event):null}/>*/}
+              <InfoBar room={room}/>
+              <Messages messages={messages} name={name}/>
+              <Input message={message} setMessage={setMessage} sendMessage={sendMessage}/>
             </div>
         </div>
     </div>)
