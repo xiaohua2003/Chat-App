e274b084b5c495748107a9ba4a586e283886c98f
xiaohua2003 <xcai3@ualberta.ca>
2021-05-01 21:06:26 -0600

    add emoji

--- client/src/components/TextContainer/TextContainer.js
+++ client/src/components/TextContainer/TextContainer.js
@@ -5,15 +5,16 @@ import './TextContainer.css'
 const TextContainer = ({ users }) => (
   <div className="textContainer">
     <div>
-      <h1>Realtime Chat Application <span role="img" aria-label="emoji">💬</span></h1>
-      <h2>Created with React, Express, Node and Socket.IO <span role="img" aria-label="emoji">❤️</span></h2>
-      <h2>Try it out right now! <span role="img" aria-label="emoji">⬅️</span></h2>
+      <h1>Realtime Chat App <span role="img" aria-label="emoji">💬</span></h1>
+      <h2>Created by Xiaohua Cai <span role="img" aria-label="emoji">👩</span></h2>
+      <h2>Created with React, Express, Node.js and Socket.IO <span role="img" aria-label="emoji">❤️</span></h2>
+      <h2>Try it out now! <span role="img" aria-label="emoji">⬅️</span></h2>
     </div>
     {
       users
         ? (
           <div>
-            <h1>People currently chatting:</h1>
+            <h1>Currently, people are chatting!</h1>
             <div className="activeContainer">
               <h2>
                 {users.map(({name}) => (
